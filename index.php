4c84acbf9f27fa8150b687135f76304bb5a8ed95
Gaz492 <gaz492@gmail.com>
2016-08-29 16:05:35 +0000

    Moved Beta to index

--- index.php
+++ index.php
@@ -41,65 +41,52 @@ require_once 'classes/getWeather.php';
   </nav>
 
   <div class="container">
-    <ul class="nav nav-tabs">
-      <li class="active"><a data-toggle="tab" href="#home">Overview</a></li>
-      <li><a data-toggle="tab" href="#lastWK">Last Week</a></li>
-      <li><a data-toggle="tab" href="#lastyr">Last Year</a></li>
-    </ul>
-
-    <div class="tab-content">
-      <div id="home" class="tab-pane fade in active">
-        <div class="container">
-          <div class="row">
-            <div class="col-md-6 info-panel">
-              <h2>12-24 Hour Forecast</h2>
-              <?php echo '<h3>Forecast: ' . $forecast . '<img src="./assets/images/forecast/' . $fc_image . '.png" alt="" style="position: absolute;"></h3>'; ?>
-            </div>
-            <div class="col-md-6 info-panel">
-              <h2>Barometer <img src="./assets/images/icons/baro_icon.png" alt"" style="position: absolute;"></h2>
-              <?php echo '<h3>Current Pressure: ' . $latestBaro['baro'] . ' hPa</h3>'; ?>
-            </div>
-          </div>
-          <div class="row">
-            <div class="col-md-6 info-panel">
-              <h2>Temperature <img src="./assets/images/icons/tem_icon.png" alt"" style="position: absolute;"></h2>
-              <?php
-              echo '<h3>Current: ' . $latestTemp['temp'] . '°C</h3>';
-              echo '<h4>Min: ' . $lowTemp['temp'] . '°C</h4>';
-              echo '<h4>Max: ' . $highTemp['temp'] . '°C</h4>';
-              ?>
-            </div>
-            <div class="col-md-6 info-panel">
-              <h2>Humidity  <img src="./assets/images/icons/humidity_icon.png" alt"" style="position: absolute;"></h2>
-              <?php
-              echo '<h3>Current: ' . $latestTemp['humid'] . '%</h3>';
-              echo '<h4>Min: ' . $lowHumid['humid'] . '%</h4>';
-              echo '<h4>Max: ' . $highHumid['humid'] . '%</h4>';
-              ?>
-            </div>
-            <div class="col-md-12 info-panel">
-              <h3>Last 24 Hours</h3>
-              <!-- <canvas id="hrAvg" width="800" height="200" style="background-color:rgba(255,255,255,1);"></canvas> -->
-              <canvas id="lastDay" width="800" height="200"></canvas>
-            </div>
-            <div class="col-md-12 info-panel">
-              <h3>Week Hourly</h3>
-              <!-- <canvas id="hrAvg" width="800" height="200" style="background-color:rgba(255,255,255,1);"></canvas> -->
-              <canvas id="lastHWeek" width="800" height="200"></canvas>
-            </div>
-          </div>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6 info-panel">
+          <h2>12-24 Hour Forecast</h2>
+          <?php echo '<h3>Forecast: ' . $forecast . '<img src="./assets/images/forecast/' . $fc_image . '.png" alt="" style="position: absolute;"></h3>'; ?>
+        </div>
+        <div class="col-md-6 info-panel">
+          <h2>Barometer <img src="./assets/images/icons/baro_icon.png" alt"" style="position: absolute;"></h2>
+          <?php echo '<h3>Current Pressure: ' . $latestBaro['baro'] . ' hPa</h3>'; ?>
         </div>
       </div>
-      <div id="lastWK" class="tab-pane fade">
-        <div class="container info-panel">
-          <h3>Last Week Average</h3>
-          <canvas id="lastWeek" width="800" height="200"></canvas>
+      <div class="row">
+        <div class="col-md-6 info-panel">
+          <h2>Temperature <img src="./assets/images/icons/tem_icon.png" alt"" style="position: absolute;"></h2>
+          <?php
+          echo '<h3>Current: ' . $latestTemp['temp'] . '°C</h3>';
+          echo '<h4>Min: ' . $lowTemp['temp'] . '°C</h4>';
+          echo '<h4>Max: ' . $highTemp['temp'] . '°C</h4>';
+          ?>
         </div>
-      </div>
-      <div id="lastyr" class="tab-pane fade">
-        <div class="container info-panel">
+        <div class="col-md-6 info-panel">
+          <h2>Humidity  <img src="./assets/images/icons/humidity_icon.png" alt"" style="position: absolute;"></h2>
+          <?php
+          echo '<h3>Current: ' . $latestTemp['humid'] . '%</h3>';
+          echo '<h4>Min: ' . $lowHumid['humid'] . '%</h4>';
+          echo '<h4>Max: ' . $highHumid['humid'] . '%</h4>';
+          ?>
+        </div>
+        <div class="col-md-12 info-panel">
+          <h3>Last 24 Hours</h3>
+          <!-- <canvas id="hrAvg" width="800" height="200" style="background-color:rgba(255,255,255,1);"></canvas> -->
+          <div id="24Hr" style="min-width: 310px; height: 400px; margin: 0 auto"></div>
+        </div>
+        <div class="col-md-12 info-panel">
+          <h3>Week Hourly</h3>
+          <!-- <canvas id="hrAvg" width="800" height="200" style="background-color:rgba(255,255,255,1);"></canvas> -->
+          <div id="weekHR" style="min-width: 310px; height: 400px; margin: 0 auto"></div>
+        </div>
+        <div class="col-md-12 info-panel">
+          <h3>Week Average</h3>
+          <!-- <canvas id="hrAvg" width="800" height="200" style="background-color:rgba(255,255,255,1);"></canvas> -->
+          <div id="week" style="min-width: 310px; height: 400px; margin: 0 auto"></div>
+        </div>
+        <div class="col-md-12 info-panel">
           <h3>Average Temperature of the last year</h3>
-          <canvas id="yearAvg" width="800" height="200"></canvas>
+          <div id="year" style="min-width: 310px; height: 400px; margin: 0 auto"></div>
         </div>
       </div>
     </div>
@@ -125,260 +112,189 @@ require_once 'classes/getWeather.php';
   </div>
 
 
+
   <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
   <!-- Include all compiled plugins (below), or include individual files as needed -->
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.2.1/Chart.bundle.min.js"></script>
+  <script src="https://code.highcharts.com/highcharts.js"></script>
+  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.14.1/moment.min.js"></script>
   <script>
-    var yearAvg = document.getElementById("yearAvg");
-    var lastWeek = document.getElementById("lastWeek");
-    var lastHWeek = document.getElementById("lastHWeek");
-    var lastDay = document.getElementById("lastDay");
-    var lastWeek = new Chart(lastWeek, {
-      type: 'line',
-      data: {
-        labels: [<?php for ($x = $countTempWK -1; $x >= 0; $x--) { echo "'" . $tempRowsWK[$x]['day'] . "', ";}?>],
-        datasets: [
-        {
-          label: "Last Week",
-          fill: false,
-          lineTension: 0.1,
-          backgroundColor: "rgba(75,192,192,0.4)",
-          borderColor: "rgba(75,192,192,1)",
-          borderCapStyle: 'butt',
-          borderDash: [],
-          borderDashOffset: 0.0,
-          borderJoinStyle: 'miter',
-          pointBorderColor: "rgba(75,192,192,1)",
-          pointBackgroundColor: "#fff",
-          pointBorderWidth: 1,
-          pointHoverRadius: 5,
-          pointHoverBackgroundColor: "rgba(75,192,192,1)",
-          pointHoverBorderColor: "rgba(220,220,220,1)",
-          pointHoverBorderWidth: 2,
-          pointRadius: 4,
-          pointHitRadius: 10,
-          data: [<?php for ($x = $countTempWK - 1; $x >= 0; $x--) { echo "'" . $tempRowsWK[$x]['avgtemp'] . "', ";}?>],
-          spanGaps: false,
-        }
-        ]
-      },
-      options: {
-        scales: {
-          yAxes: [{
-            ticks: {
-              beginAtZero:false
-            },
-            gridLines:{
-              color:"rgba(180, 180, 180, 1)",
-              zeroLineColor:"rgba(180, 180, 180, 1)"
-            }
-          }],
-          xAxes:[{
-            type: 'time',
-            time: {
-              format: "YYYY-MM-DD-dddd",
-              unit: 'day',
-              unitStepSize: 7,
-              tooltipFormat: 'DD dddd'
-            },
-            scaleLabel: {
-              display: true,
-              labelString: 'Day'
-            },
-            gridLines:{
-              color:"rgba(180, 180, 180, 1)",
-              zeroLineColor:"rgba(180, 180, 180, 1)"
-            }
-          }],
-        }
-      }
-    });
+    $(function () {
 
-    var lastHWeek = new Chart(lastHWeek, {
-      type: 'line',
-      data: {
-        labels: [<?php for ($x = $countTempHWK -1; $x >= 0; $x--) { echo "'" . $tempRowsHWK[$x]['time'] . "', ";}?>],
-        datasets: [
-        {
-          label: "Week Hourly",
-          fill: false,
-          lineTension: 0.1,
-          backgroundColor: "rgba(75,192,192,0.4)",
-          borderColor: "rgba(75,192,192,1)",
-          borderCapStyle: 'butt',
-          borderDash: [],
-          borderDashOffset: 0.0,
-          borderJoinStyle: 'miter',
-          pointBorderColor: "rgba(75,192,192,1)",
-          pointBackgroundColor: "#fff",
-          pointBorderWidth: 1,
-          pointHoverRadius: 5,
-          pointHoverBackgroundColor: "rgba(75,192,192,1)",
-          pointHoverBorderColor: "rgba(220,220,220,1)",
-          pointHoverBorderWidth: 2,
-          pointRadius: 4,
-          pointHitRadius: 10,
-          data: [<?php for ($x = $countTempHWK -1; $x >= 0; $x--) { echo "'" . $tempRowsHWK[$x]['avgtemp'] . "', ";}?>],
-          spanGaps: false,
-        }
-        ]
-      },
-      options: {
-        responsive: true,
-        scales: {
-          yAxes: [{
-            ticks: {
-              beginAtZero:false
-            },
-            gridLines:{
-              color:"rgba(180, 180, 180, 1)",
-              zeroLineColor:"rgba(180, 180, 180, 1)"
-            }
-          }],
-          xAxes:[{
-            display: true,
-            type: 'time',
-            // time: {
-            //   format: "YYYY-MM-DD HH:mm",
-            //   unit: 'hour',
-            //   unitStepSize: 5,
-            //   tooltipFormat: 'HH:mm'
-            // },
-            scaleLabel: {
-              display: true,
-              labelString: 'Hours'
-            },
-            gridLines:{
-              color:"rgba(180, 180, 180, 1)",
-              zeroLineColor:"rgba(180, 180, 180, 1)"
-            }
-          }],
-        }
-      }
-    });
-    
-    var lastDay = new Chart(lastDay, {
-      type: 'line',
-      data: {
-        labels: [<?php for ($x = $countTemp -1; $x >= 0; $x--) { echo "'" . $tempRows[$x]['time'] . "', ";}?>],
-        datasets: [
-        {
-          label: "Last 24 Hours",
-          fill: false,
-          lineTension: 0.1,
-          backgroundColor: "rgba(75,192,192,0.4)",
-          borderColor: "rgba(75,192,192,1)",
-          borderCapStyle: 'butt',
-          borderDash: [],
-          borderDashOffset: 0.0,
-          borderJoinStyle: 'miter',
-          pointBorderColor: "rgba(75,192,192,1)",
-          pointBackgroundColor: "#fff",
-          pointBorderWidth: 1,
-          pointHoverRadius: 5,
-          pointHoverBackgroundColor: "rgba(75,192,192,1)",
-          pointHoverBorderColor: "rgba(220,220,220,1)",
-          pointHoverBorderWidth: 2,
-          pointRadius: 4,
-          pointHitRadius: 10,
-          data: [<?php for ($x = $countTemp -1; $x >= 0; $x--) { echo "'" . $tempRows[$x]['avgtemp'] . "', ";}?>],
-          spanGaps: false,
-        }
-        ]
-      },
-      options: {
-        scales: {
-          yAxes: [{
-            ticks: {
-              beginAtZero:false
-            },
-            gridLines:{
-              color:"rgba(180, 180, 180, 1)",
-              zeroLineColor:"rgba(180, 180, 180, 1)"
-            }
-          }],
-          xAxes:[{
-            type: 'time',
-            // time: {
-            //   format: "YYYY-MM-DD HH:mm",
-            //   unit: 'hour',
-            //   unitStepSize: 1,
-            //   tooltipFormat: 'HH:mm'
-            // },
-            scaleLabel: {
-              display: true,
-              labelString: 'Hours'
-            },
-            gridLines:{
-              color:"rgba(180, 180, 180, 1)",
-              zeroLineColor:"rgba(180, 180, 180, 1)"
-            }
-          }],
-        }
-      }
-    });
+      Highcharts.setOptions({
+        chart: {
 
-    var yearAvg = new Chart(yearAvg, {
-      type: 'line',
-      data: {
-        labels: [<?php for ($x = $countTempYR -1; $x >= 0; $x--) { echo "'" . $tempRowsYR[$x]['month'] . "', ";} ?>],
-        datasets: [
-        {
-          label: "Average Temperature",
-          fill: false,
-          lineTension: 0.1,
-          backgroundColor: "rgba(75,192,192,0.4)",
-          borderColor: "rgba(75,192,192,1)",
-          borderCapStyle: 'butt',
-          borderDash: [],
-          borderDashOffset: 0.0,
-          borderJoinStyle: 'miter',
-          pointBorderColor: "rgba(75,192,192,1)",
-          pointBackgroundColor: "#fff",
-          pointBorderWidth: 1,
-          pointHoverRadius: 5,
-          pointHoverBackgroundColor: "rgba(75,192,192,1)",
-          pointHoverBorderColor: "rgba(220,220,220,1)",
-          pointHoverBorderWidth: 2,
-          pointRadius: 4,
-          pointHitRadius: 10,
-          data: [<?php for ($x = $countTempYR -1; $x >= 0; $x--) { echo "'" . $tempRowsYR[$x]['avgtemp'] . "', ";} ?>],
-          spanGaps: false,
-        }
-        ]
-      },
-      options: {
-        scales: {
-          yAxes: [{
-            ticks: {
-              beginAtZero:false
-            },
-            gridLines:{
-              color:"rgba(180, 180, 180, 1)",
-              zeroLineColor:"rgba(180, 180, 180, 1)"
+        },
+        credits: {
+          enabled: false
+        },
+        tooltip: {
+          crosshairs: true,
+          shared: true
+        },
+        plotOptions: {
+          spline: {
+            marker: {
+              radius: 4,
+              lineColor: '#4bc0c0',
+              lineWidth: 1
             }
-          }],
-          xAxes:[{
-            type: "time",
-            time: {
-              format: "MMMM",
-              unit: 'month',
-              unitStepSize: 1,
-              tooltipFormat: 'MMM YYYY'
-            },
-            scaleLabel: {
-              display: true,
-              labelString: 'Month'
-            },
-            gridLines:{
-              color:"rgba(180, 180, 180, 1)",
-              zeroLineColor:"rgba(180, 180, 180, 1)"
+          }
+        },
+      });
+
+      $('#24Hr').highcharts({
+        chart: {
+          type: 'line'
+        },
+        title: {
+          text: '24 Hour Average Temprature'
+        },
+        subtitle: {
+          text: 'Source: Home Sensor'
+        },
+        xAxis: {
+          type: 'datetime',
+          labels: {
+            formatter: function() {
+              return moment(this.value).format("Do HH:mm");
             }
-          }],
-        }
-      }
+          },
+          categories: [<?php for ($x = $countTemp - 1; $x >= 0; $x--) { echo "'" . $tempRows[$x]['time'] . "', ";}?>]
+        },
+        yAxis: {
+          title: {
+            text: 'Temperature'
+          },
+          labels: {
+            formatter: function () {
+              return this.value + '°c';
+            }
+          }
+        },
+        series: [{
+          name: 'Home',
+          marker: {
+            symbol: 'circle'
+          },
+          data: [<?php for ($x = $countTemp -1; $x >= 0; $x--) { echo $tempRows[$x]['avgtemp'] . ", ";}?>]
+        }]
+      });
+
+      $('#weekHR').highcharts({
+        chart: {
+          type: 'line'
+        },
+        title: {
+          text: 'Week Hour Average Temprature'
+        },
+        subtitle: {
+          text: 'Source: Home Sensor'
+        },
+        xAxis: {
+          type: 'datetime',
+          labels: {
+            formatter: function() {
+              return moment(this.value).format("MMM-Do HH:mm");
+            }
+          },
+          categories: [<?php for ($x = $countTempHWK -1; $x >= 0; $x--) { echo "'" . $tempRowsHWK[$x]['time'] . "', ";}?>]
+        },
+        yAxis: {
+          title: {
+            text: 'Temperature'
+          },
+          labels: {
+            formatter: function () {
+              return this.value + '°c';
+            }
+          }
+        },
+        series: [{
+          name: 'Home',
+          marker: {
+            symbol: 'circle'
+          },
+          data: [<?php for ($x = $countTempHWK -1; $x >= 0; $x--) { echo $tempRowsHWK[$x]['avgtemp'] . ", ";}?>]
+        }]
+      });
+
+      $('#week').highcharts({
+        chart: {
+          type: 'line'
+        },
+        title: {
+          text: 'Week Hour Average Temprature'
+        },
+        subtitle: {
+          text: 'Source: Home Sensor'
+        },
+        xAxis: {
+          type: 'datetime',
+          labels: {
+            formatter: function() {
+              return moment(this.value).format("dddd Do");
+            }
+          },
+          categories: [<?php for ($x = $countTempWK -1; $x >= 0; $x--) { echo "'" . $tempRowsWK[$x]['day'] . "', ";}?>]
+        },
+        yAxis: {
+          title: {
+            text: 'Temperature'
+          },
+          labels: {
+            formatter: function () {
+              return this.value + '°c';
+            }
+          }
+        },
+        series: [{
+          name: 'Home',
+          marker: {
+            symbol: 'circle'
+          },
+          data: [<?php for ($x = $countTempWK -1; $x >= 0; $x--) { echo $tempRowsWK[$x]['avgtemp'] . ", ";}?>]
+        }]
+      });
+
+      $('#year').highcharts({
+        chart: {
+          type: 'line'
+        },
+        title: {
+          text: 'Week Hour Average Temprature'
+        },
+        subtitle: {
+          text: 'Source: Home Sensor'
+        },
+        xAxis: {
+          type: 'datetime',
+          labels: {
+            formatter: function() {
+              return moment(this.value).format("MMMM");
+            }
+          },
+          categories: [<?php for ($x = $countTempYR -1; $x >= 0; $x--) { echo "'" . $tempRowsYR[$x]['month'] . "', ";} ?>]
+        },
+        yAxis: {
+          title: {
+            text: 'Temperature'
+          },
+          labels: {
+            formatter: function () {
+              return this.value + '°c';
+            }
+          }
+        },
+        series: [{
+          name: 'Home',
+          marker: {
+            symbol: 'circle'
+          },
+          data: [<?php for ($x = $countTempYR -1; $x >= 0; $x--) { echo $tempRowsYR[$x]['avgtemp'] . ", ";} ?>]
+        }]
+      });
     });
   </script>
 
