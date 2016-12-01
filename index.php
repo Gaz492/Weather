fbaf1271b41a1a7db1e76b4cbcfdd0d4954c43ff
Gareth <gaz492@gmail.com>
2016-12-01 17:49:55 +0000

    Cleanup

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
- error_reporting(-1);
- ini_set('display_errors', 'On');
+// error_reporting(-1);
+// ini_set('display_errors', 'Off');
 require_once 'classes/getWeather.php';
 ?>
 <!DOCTYPE html>
--- update.php
+++ update.php
@@ -1,6 +1,6 @@
 <?php
-error_reporting(-1);
-ini_set('display_errors', 'On');
+// error_reporting(-1);
+// ini_set('display_errors', 'Off');
 parse_str(file_get_contents("php://input"), $_POST);
 
 require_once './include/config.php';
@@ -12,43 +12,33 @@ try {
   echo 'Error: ' . $e->getMessage();
 }
 
-$mac = "";
-$humidity = "";
-$temprature = "";
-$channel = "";
-$barometer = "";
-$wForecast = "";
+if( isset($_POST['mac']) ){
+    // $channel = $_POST['ch'];
 
-$mac = $_POST['mac'];
-$humidity = $_POST['oh'];
-$temprature = $_POST['ot'];
-$channel = $_POST['ch'];
-$barometer = $_POST['baro'];
-$wForecast = $_POST['wfor'];
+    if( $_POST['mac'] == $macaddress){
+        if( isset($_POST['baro']) )
+        {
+            $stmt = $pdo->prepare ("INSERT INTO cust_weatherOther (baro, forecast) VALUES (:baro, :forecast)");
+            $stmt -> bindParam(':baro', $_POST['baro']);
+            $stmt -> bindParam(':forecast', $_POST['wfor']);
+            $stmt -> execute();
+        }
+        elseif( isset($_POST['ot']) )
+        {
+            $stmt = $pdo->prepare ("INSERT INTO cust_weatherTemp (humid, temp) VALUES (:humid, :temp)");
+            $stmt -> bindParam(':humid', $_POST['oh']);
+            $stmt -> bindParam(':temp', $_POST['ot']);
+            $stmt -> execute();
+        }
+        else
+        {
+            http_response_code(400);
+            echo 'ERROR: Bad Request';
+        }
 
-if( $mac == $macaddress){
-    if( isset($barometer) )
-    {
-        $stmt = $pdo->prepare ("INSERT INTO cust_weatherOther (baro, forecast) VALUES (:baro, :forecast)");
-        $stmt -> bindParam(':baro', $barometer);
-        $stmt -> bindParam(':forecast', $wForecast);
-        $stmt -> execute();
     }
-    elseif( isset($temprature) )
-    {
-        $stmt = $pdo->prepare ("INSERT INTO cust_weatherTemp (humid, temp) VALUES (:humid, :temp)");
-        $stmt -> bindParam(':humid', $humidity);
-        $stmt -> bindParam(':temp', $temprature);
-        $stmt -> execute();
+    else {
+        http_response_code(401);
+        echo 'ERROR: Unauthorized macaddress ' . $mac;
     }
-    else
-    {
-        http_response_code(400);
-        echo 'ERROR: Bad Request';
-    }
-    
-}
-else {
-    http_response_code(400);
-    echo 'ERROR: Bad Request';
 }
